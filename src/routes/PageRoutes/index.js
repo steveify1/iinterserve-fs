20cb3bc93a92796d40881cb7f19edebc96389ddf
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2019-12-08 14:28:55 +0100

    chore(eslint):setup eslint - setup eslint - extend the airbnb style guide - set rules in .eslintrc.json - fix linted code in .js files [Finishes #2]

--- src/routes/PageRoutes/index.js
+++ src/routes/PageRoutes/index.js
@@ -1,25 +1,25 @@
-const { Router } = require("express");
+const { Router } = require('express');
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.render("index.ejs");
+router.get('/', (req, res) => {
+  res.render('index.ejs');
 });
 
-router.get("/services", (req, res) => {
-  res.render("services.ejs");
+router.get('/services', (req, res) => {
+  res.render('services.ejs');
 });
 
-router.get("/projects", (req, res) => {
-  res.render("projects.ejs");
+router.get('/projects', (req, res) => {
+  res.render('projects.ejs');
 });
 
-router.get("/contact", (req, res) => {
-  res.render("contact.ejs");
+router.get('/contact', (req, res) => {
+  res.render('contact.ejs');
 });
 
-router.get("/about", (req, res) => {
-  res.render("about.ejs");
+router.get('/about', (req, res) => {
+  res.render('about.ejs');
 });
 
 module.exports = router;
