435a1e4eb625fe4516837a9ff643cefa6446dd0d
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2019-12-09 13:18:25 +0100

    chore(release):prepare first release - secure the application - compress http responses [Finishes #18]

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 const routes = require('./routes/index.js');
 
 const app = express();
 
 // MIDDLEWARES
 app.use(morgan());
+
+// HELMET - request protection
+app.use(helmet());
+
+// XSS Prevention
+app.use(xss());
+
+// Rate Limiting
+const limitConfig = {
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP. Please, try again in an hour.'
+};
+app.use(rateLimit(limitConfig));
+
+// COMPRESS SERVER TEXT-BASED RESPONSES
+app.use(compression());
 
 // VIEW ENGINE & VIEWS DIRECTORY
 app.set('views', path.join(__dirname, 'views'));
