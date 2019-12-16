122cda3864d3413031c08487e8b4cdaeced1d92b
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2019-12-16 15:08:11 +0100

    refactor(layout):update view layout [Finishes #23]

--- src/routes/PageRoutes/index.js
+++ src/routes/PageRoutes/index.js
@@ -1,25 +1,41 @@
 const { Router } = require('express');
 
 const router = Router();
+const brand = 'InterserveConstruct';
 
 router.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index.ejs', {
+    // eslint-disable-next-line comma-dangle
+    title: `${brand} - Top construction firm in Nigeria`
+  });
 });
 
 router.get('/services', (req, res) => {
-  res.render('services.ejs');
+  res.render('services.ejs', {
+    // eslint-disable-next-line comma-dangle
+    title: `${brand} - Services porta cabin, construction, warehouse, solar panel`
+  });
 });
 
 router.get('/projects', (req, res) => {
-  res.render('projects.ejs');
+  res.render('projects.ejs', {
+    // eslint-disable-next-line comma-dangle
+    title: `${brand} - Our Projects`
+  });
 });
 
 router.get('/contact', (req, res) => {
-  res.render('contact.ejs');
+  res.render('contact.ejs', {
+    // eslint-disable-next-line comma-dangle
+    title: `${brand} - Contact`
+  });
 });
 
 router.get('/about', (req, res) => {
-  res.render('about.ejs');
+  res.render('about.ejs', {
+    // eslint-disable-next-line comma-dangle
+    title: `${brand} - About us`
+  });
 });
 
 module.exports = router;
